30f22ceee54b088ec7238669383826a873123319
Aayushmaan <aayushmaan@gmail.com>
2026-06-24 15:37:09 +0530

    check vercel frontend deploy

--- server.js
+++ server.js
@@ -18,7 +18,8 @@ app.use(
       "http://127.0.0.1:5500",
       "http://localhost:5500",
       "http://127.0.0.1:5173",
-      "http://localhost:5173"
+      "http://localhost:5173",
+      "https://joule-ai.vercel.app"
     ],
     methods: ["GET", "POST", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
