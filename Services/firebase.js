fe1a4ab2397ff58e5ca4975e278e573efd3a0bc0
Aayushmaan <aayushmaan@gmail.com>
2026-06-24 15:31:37 +0530

    read firebase service account from env var

--- Services/firebase.js
+++ Services/firebase.js
@@ -1,5 +1,14 @@
 import { initializeApp, cert, getApps, getApp } from "firebase-admin/app";
-import serviceAccount from "../config/firebase-service-account.json" with { type: "json" };
+import { createRequire } from "module";
+
+let serviceAccount;
+
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+} else {
+  const require = createRequire(import.meta.url);
+  serviceAccount = require("../config/firebase-service-account.json");
+}
 
 const app = getApps().length
   ? getApp()
